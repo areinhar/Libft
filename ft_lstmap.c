7459bc0de98bbcc837d3412aafe23f7f47100147
areinhar <areinhar@student.s19.be>
2021-02-10 04:16:48 +0100

    ft_lstmap: The del parameter is allowed to be empty, or else the test fails. Should be ok now

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: areinhar <areinhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/06 20:19:44 by areinhar          #+#    #+#             */
-/*   Updated: 2021/02/10 02:29:08 by areinhar         ###   ########.fr       */
+/*   Updated: 2021/02/10 04:14:06 by areinhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,15 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_elem;
 	void	*result;
 
-	if (!lst || !f || !del)
+	if (!lst || !f)
 		return (NULL);
 	new_lst = NULL;
-	result = NULL;
 	while (lst)
 	{
 		result = f(lst->content);
 		if (!(result) || !(new_elem = ft_lstnew(result)))
 		{
-			(*del)(result);
+			del(result);
 			ft_lstclear(&new_lst, del);
 			break ;
 		}
